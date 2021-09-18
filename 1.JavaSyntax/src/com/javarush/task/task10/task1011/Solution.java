7b9886272510f1b9765c0482a39650a3f82e15e5
Anton-Protasov <89267016232@mail.ru>
2021-09-18 11:30:38 +0300

    #1011 (substring)

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = "Я хочу большую зарплату";
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.substring(i));
+        }
+    }
+
+}
+
