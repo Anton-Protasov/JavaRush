94667794c870f838a6da159b2e153e5bcf2ecd5d
Anton-Protasov <89267016232@mail.ru>
2021-10-21 23:26:30 +0300

    #1916 (To compare lines in 2 files)

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task19.task1916;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Отслеживаем изменения
+*/
+
+public class Solution {
+    public static List<LineItem> lines = new ArrayList<LineItem>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        FileReader file1 = new FileReader(bufferedReader.readLine());
+        FileReader file2 = new FileReader(bufferedReader.readLine());
+
+        List<String> fileList1 = new ArrayList<>();
+        BufferedReader buFile1 = new BufferedReader(file1);
+        while (buFile1.ready()) {
+            fileList1.add(buFile1.readLine());
+        }
+
+        List<String> fileList2 = new ArrayList<>();
+        BufferedReader buFile2 = new BufferedReader(file2);
+        while (buFile2.ready()) {
+            fileList2.add(buFile2.readLine());
+        }
+
+        int n1 = 0;
+        int n2 = 0;
+        while (n1 < fileList1.size() && n2 < fileList2.size()) {
+            if (fileList1.get(n1).equals(fileList2.get(n2))) {
+                lines.add(new LineItem(Type.SAME, fileList1.get(n1)));
+                n1++;
+                n2++;
+            } else if (!fileList2.contains(fileList1.get(n1))) {
+                lines.add(new LineItem(Type.REMOVED, fileList1.get(n1)));
+                n1++;
+            } else if (fileList1.contains(fileList2.get(n2))) {
+                lines.add(new LineItem(Type.ADDED, fileList2.get(n2)));
+                n2++;
+            }
+        }
+        if (n1 < fileList1.size()) {
+            lines.add(new LineItem(Type.REMOVED, fileList1.get(n1)));
+        } else if (n2 < fileList2.size()) {
+            lines.add(new LineItem(Type.ADDED, fileList2.get(n2)));
+        }
+
+        bufferedReader.close();
+        file1.close();
+        file2.close();
+        buFile1.close();
+        buFile2.close();
+    }
+
+    public static enum Type {
+        ADDED,        //добавлена новая строка
+        REMOVED,      //удалена строка
+        SAME          //без изменений
+    }
+
+    public static class LineItem {
+        public Type type;
+        public String line;
+
+        public LineItem(Type type, String line) {
+            this.type = type;
+            this.line = line;
+        }
+    }
+}
